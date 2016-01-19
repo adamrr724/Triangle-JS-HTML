48f25bc823a236d4835a07d68d1f5f7c876a3721
Adam Ross Russell and Conner Belvin <adamrr724@yahoo.com>
2016-01-19 13:03:00 -0800

    initial commit for triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,42 @@
-var leapYear = function(year) {
-  if((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-    return true;
-  } else {
+var triangle = function(side1, side2, side3) {
+
+  if ( isNaN(side1) || isNaN(side2) || isNaN(side3)) {
     return false;
+  } else if ((side1 + side2 <= side3) || (side3 + side2 <= side2) || (side1 + side3 <= side2)) {
+    return false;
+  } else if((side1 === side2) && (side1 === side3)) {
+    return true;
+  } else if((side1 === side2) || (side2 === side3) || (side1 === side3)) {
+    return true;
+  } else if((side1 !== side2) && (side2 !== side3) && (side1 !== side3)) {
+    return true;
   }
+
 };
 
 $(document).ready(function(event) {
-    $("form#leap-year").submit(function(event){
-      var year = parseInt($("input#year").val());
-      var result = leapYear(year);
+    $("form#triangle").submit(function(event){
+      var side1 = parseInt($("input#side1").val());
+      var side2 = parseInt($("input#side2").val());
+      var side3 = parseInt($("input#side3").val());
+      var result = triangle(side1, side2, side3);
 
-      $(".year").text(year);
-
-    if(!result) {
-      $(".not").text("not");
-    } else {
-      $(".not").text("");
+    if ( isNaN(side1) || isNaN(side2) || isNaN(side3)) {
+      alert("Please write a valid input");
+      $(".type").text("not a");
+    } else if((side1 === side2) && (side1 === side3)) {
+      $(".type").text("an equilateral");
+    } else if (!result) {
+      $(".type").text("not a");
+    } else if((side1 === side2) || (side2 === side3) || (side1 === side3)) {
+      $(".type").text("an isosceles");
+    } else if((side1 !== side2) && (side2 !== side3) && (side1 !== side3)) {
+      $(".type").text("a scalene");
     }
+
+
+
+
 
     $("#result").show();
     event.preventDefault();
