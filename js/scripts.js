801edb94c7dde75263e9dc92937ff23879430c10
Adam Ross Russell and Conner Belvin <c9er99@gmail.com>
2016-01-19 14:19:35 -0800

    it worked

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,24 @@
-var triangle = function(side1, side2, side3) {
-
-  if ( isNaN(side1) || isNaN(side2) || isNaN(side3)) {
-    return false;
-  } else if ((side1 + side2 <= side3) || (side3 + side2 <= side2) || (side1 + side3 <= side2)) {
-    return false;
-  } else if((side1 === side2) && (side1 === side3)) {
-    return true;
-  } else if((side1 === side2) || (side2 === side3) || (side1 === side3)) {
-    return true;
-  } else if((side1 !== side2) && (side2 !== side3) && (side1 !== side3)) {
-    return true;
-  }
-
-};
+// var code = function(message) {
+//   if (square === rounded) {
+//     return true;
+//   }
+//
+// };
 
 $(document).ready(function(event) {
-    $("form#triangle").submit(function(event){
-      var side1 = parseInt($("input#side1").val());
-      var side2 = parseInt($("input#side2").val());
-      var side3 = parseInt($("input#side3").val());
-      var result = triangle(side1, side2, side3);
-
-    if ( isNaN(side1) || isNaN(side2) || isNaN(side3)) {
-      alert("Please write a valid input");
-      $(".type").text("not a");
-    } else if((side1 === side2) && (side1 === side3)) {
-      $(".type").text("an equilateral");
-    } else if (!result) {
-      $(".type").text("not a");
-    } else if((side1 === side2) || (side2 === side3) || (side1 === side3)) {
-      $(".type").text("an isosceles");
-    } else if((side1 !== side2) && (side2 !== side3) && (side1 !== side3)) {
-      $(".type").text("a scalene");
-    }
-
+    $("form#code").submit(function(event){
+      var message = $("input#message").val();
+      var messagefix = message.replace(/[^A-Za-z]/g,"");
+      console.log("the input for messagefix is" + messagefix);
+    //   var x = messagefix.length;
+    //   var square = Math.sqrt(x);
+    //   var rounded = Math.round(square);
+    //   var result = code(message);
+    //
+    //
+    // if (true) {
+    //   $("#result").after();
+    // }
 
 
 
--- js/specs.js
+++ js/specs.js
@@ -1,18 +1,4 @@
-describe('triangle', function() {
-  it("is false if the sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side ", function() {
-    expect(triangle(2,2,6)).to.equal(false);
+describe('code', function() {
+  it("is true if the number of characters is a perfect square", function() {
+    expect(code("helloadam")).to.equal(true);
   });
-  it("is true if all sides are equal", function() {
-    expect(triangle(4,4,4)).to.equal(true);
-  });
-  it("is true if exactly 2 sides are equal", function() {
-    expect(triangle(3,3,5)).to.equal(true);
-  });
-  it("is true if no sides are equal", function() {
-    expect(triangle(2,4,5)).to.equal(true);
-  });
-  it("is false for a string", function() {
-    expect(triangle(NaN,3,4)).to.equal(false);
-  })
-
-});
