de56b0e8db6120d8be62af53f58ec28b7b85c696
Adam Ross Russell and Conner Belvin <adamrr724@yahoo.com>
2016-01-19 15:56:03 -0800

    add functions to create code

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,35 @@
-// var code = function(message) {
-//   if (square === rounded) {
-//     return true;
-//   }
-//
-// };
+
+
 
 $(document).ready(function(event) {
     $("form#code").submit(function(event){
+
+
       var message = $("input#message").val();
-      var messagefix = message.replace(/[^A-Za-z]/g,"");
-      console.log("the input for messagefix is" + messagefix);
-    //   var x = messagefix.length;
-    //   var square = Math.sqrt(x);
-    //   var rounded = Math.round(square);
-    //   var result = code(message);
-    //
-    //
-    // if (true) {
-    //   $("#result").after();
-    // }
+      var messagefix = message.toLowerCase().replace(/[^A-Za-z]/g,"");
+      var x = messagefix.length;
+      var square = Math.sqrt(x);
+      var rounded = Math.ceil(square);
+      var string = messagefix.split("");
+      var incmessage = "";
+      var finalmessage = "";
+
+      for(var i = 0; i < rounded; i++) {
+        for(var j = i; j < x; j += rounded){
+          incmessage += string[j];
+        }
+      }
+
+      for(var i = 0; i < x; i++) {
+        finalmessage += incmessage[i];
+        if ((i + 1) % 5 === 0) {
+          finalmessage += " ";
+        }
+      }
 
 
 
-
+    $(".result").text(finalmessage);
     $("#result").show();
     event.preventDefault();
 
